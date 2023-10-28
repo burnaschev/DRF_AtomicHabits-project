34fd5620df8b57af61e98727e1274e58c0795a96
Artur Bur <informal201144@gmail.com>
2023-10-28 14:59:33 +0500

    fix

--- habits/tasks.py
+++ habits/tasks.py
@@ -13,10 +13,13 @@ bot = TeleBot(settings.TELEGRAM_BOT_API_KEY, threaded=False)
 def send_notifications():
     habits = Habit.objects.all()
     current_time = datetime.now().time()
+    current_date = datetime.now().date()
 
     for habit in habits:
         if habit.time == current_time:
             if habit.user.chat_id:
-                bot.send_message(chat_id=habit.user.chat_id, text=f'Пришло время привычки: {habit}')
-                habit.send_time = datetime.now()
-                habit.save()
+                days_passed = (current_date - habit.send_time.date()).days
+                if days_passed >= habit.period:
+                    bot.send_message(chat_id=habit.user.chat_id, text=f'Вам нужно сделать {habit.action} в {habit.place} в {habit.time}')
+                    habit.send_time = datetime.now()
+                    habit.save()
